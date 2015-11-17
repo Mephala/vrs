e1f6d1f05cf67c301d5c605ec6ce565186013213
gokhanabi <gokhanabi@gokhanabi-laptop>
2015-11-17 23:31:10 +0200

    initial commit

---
+++ vrs/src/main/java/vrs/controller/HomeController.java
@@ -0,0 +1,30 @@
+package vrs.controller;
+
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import vrs.util.VRSUtils;
+
+@Controller
+public class HomeController {
+
+	private Logger logger = Logger.getLogger(getClass());
+
+	@RequestMapping(value = "/homepage")
+	@ResponseBody
+	public Object showHome(HttpServletRequest request, HttpServletResponse response) {
+		// ModelAndView mav = new ModelAndView("home");
+		logger.info("Received request from:" + VRSUtils.getClientIpAddr(request));
+
+		// return mav;
+		return UUID.randomUUID().toString();
+	}
+
+}
